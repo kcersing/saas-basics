5840a0014f0de0a8886f4b7b7c7ad2f6121be663
kcersing <wt4@live.cn>
2024-03-26 18:43:31 +0800

    订单

--- app/admin/idl_gen/admin/order.thrift
+++ app/admin/idl_gen/admin/order.thrift
@@ -15,47 +15,45 @@ struct OrderItem {
 }
 struct CreateOrderReq {
     1: required i64 user_id
-    2: required string address
+    2: required list<i64> sell_id
     3: required i64 product_id
     4: required i64 stock_num
     5: required i64 venue_id
-    6: i64 status
-}
+    6: required i64 create_id
 
-struct CreateOrderResp {
-    255: base.BaseResp BaseResp
 }
 
 struct CancelOrderReq {
     1: required i64 order_id
 }
 
-struct CancelOrderResp {
-    255: base.BaseResp BaseResp
-}
-
 struct ListOrderReq {
-    1: required i64 user_id
-    2: optional i64 status
-}
+    1:  optional i64 page (api.raw = "page")
+    2:  optional i64 limit (api.raw = "limit")
+    3:  optional i64 member (api.raw = "member")
+    4:  optional list<i64> sell (api.raw = "sell")
+    5:  optional list<i64> product (api.raw = "product")
+    6:  optional list<i64> venue (api.raw = "venue")
 
-struct ListOrderResp {
-    1: list<OrderItem> orders
-    255: base.BaseResp BaseResp
 }
 
 struct GetOrderByIdReq {
     1: required i64 order_id
 }
 
-struct GetOrderByIdResp {
+struct UpdateOrderReq {
     1: OrderItem order
-    255: base.BaseResp BaseResp
+
 }
 
 service OrderService {
-    CreateOrderResp CreateOrder(1: CreateOrderReq req) // 创建订单
-    CancelOrderResp CancelOrder(1: CancelOrderReq req) // 取消订单
-    ListOrderResp ListOrder(1: ListOrderReq req) // 订单列表
-    GetOrderByIdResp GetOrderById(1: GetOrderByIdReq req) // 订单详情
+    base.NilResponse CreateOrder(1: CreateOrderReq req)  (api.post = "/api/admin/order/create")// 创建订单
+
+    base.NilResponse UpdateUser(1: UpdateOrderReq req) (api.post = "/api/admin/order/update")
+
+    base.NilResponse CancelOrder(1: base.IDReq req)  (api.post = "/api/admin/order/cancel")// 取消订单
+
+    base.NilResponse ListOrder(1: ListOrderReq req )(api.get = "/api/admin/order/list") // 订单列表
+
+    base.NilResponse GetOrderById(1: GetOrderByIdReq req) (api.get = "/api/admin/order/info") // 订单详情
 }
