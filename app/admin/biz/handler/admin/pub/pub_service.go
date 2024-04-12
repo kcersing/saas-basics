15be09f1f56043a07b66d2a1e74bd2b67dffc4a5
kcersing <wt4@live.cn>
2024-04-12 18:02:12 +0800

    上传接口

--- app/admin/biz/handler/admin/pub/pub_service.go
+++ app/admin/biz/handler/admin/pub/pub_service.go
@@ -18,7 +18,11 @@ import (
 // Upload .
 // @router /api/pub/upload/ [POST]
 func Upload(ctx context.Context, c *app.RequestContext) {
-	file, _ := c.FormFile("file")
+	file, err := c.FormFile("file")
+	if err != nil {
+		utils.SendResponse(c, errno.ConvertErr(err), nil, 0, "")
+		return
+	}
 	nowTime := time.Now()
 	filename := minio.NewFileName(0, nowTime.UnixMicro())
 	file.Filename = filename + path.Ext(file.Filename)
@@ -29,7 +33,9 @@ func Upload(ctx context.Context, c *app.RequestContext) {
 	}
 	url := minio.URLconvert(ctx, c, config.GlobalServerConfig.Minio.ImgBucketName+"/"+uploadinfo.Key)
 	utils.SendResponse(c, errno.Success, map[string]string{
-		"url": url,
+		"name": uploadinfo.Key,
+		"url":  url,
+		"path": config.GlobalServerConfig.Minio.ImgBucketName + "/" + uploadinfo.Key,
 	}, 1, "")
 
 	return
